1956d2ffdcc3c01ca27841d88034a9d8e165dc09
TLAllen662 <tlallen1419@gmail.com>
2025-09-18 22:15:16 -0500

    Implement constructors and methods for Person, Student, and Staff classes

--- school.py
+++ school.py
@@ -1,11 +1,34 @@
 class Person:
     # Delete pass and add your code here
-    pass
+    def __init__ (self, name, age, country):
+        self.name = name
+        self.age = age
+        self.country = country 
+    def __str__ (self):
+        return f"{self.name} is {self.age} years old and lives in {self.country}."
+    
+person_1 = Person("Ava", 28, "USA")
 
 class Student(Person):
     # Delete pass and add your code here
-    pass
+    def __init__ (self,name, age, country, major, gpa):
+        super().__init__(name, age, country)
+        self.major = major
+        self.gpa = gpa
+        def study(self):
+            return f"{self.name} is studying {self.major} with a current GPA of {self.age}."
+        
+student_1 = Student("Jake", 19, "England", "Computer Science", 3.85)
+
+
 
 class Staff(Person):
     # Delete pass and add your code here
-    pass
+    def __init__ (self, name, age, country, position, department):
+        super().__init__(name, age, country)
+        self.position = position
+        self.department = department
+        def work(self):
+            return f"{self.name} works as a {self.position} in the {self.department} department."
+        
+staff_1 = Staff("Robert", 44, "USA", "Professor", "History")
